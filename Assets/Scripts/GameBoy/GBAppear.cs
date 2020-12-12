2a8ade754de9d01eabf347a1f88f50bc98be936d
Leon Portius <Pluto-is-not-too-small@gmx.de>
2020-12-12 23:25:38 +0100

    gamebos has trigger, onEnter an a popup animation

---
+++ Assets/Scripts/GameBoy/GBAppear.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GBAppear : MonoBehaviour
+{
+    [SerializeField] Animator gba;
+    [SerializeField] AnimationClip anim;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gba.Play(anim.name);
+        }
+    }
+}
