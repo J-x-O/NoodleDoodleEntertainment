113f6d1dc50c18d36dcbe70cd3a417d54615acba
Ibrahim Kirschstein <ikirschs@hs-mittweida.de>
2020-12-13 16:24:51 +0100

    Springs added

--- Assets/Scripts/Level/Spring.cs
+++ Assets/Scripts/Level/Spring.cs
@@ -6,13 +6,14 @@ public class Spring : MonoBehaviour
 {
     [SerializeField] GameObject Player;
     [SerializeField] float springModifier;
+    [SerializeField] float springBase;
     private float playerSpeed;
 
 
     public void OnTriggerEnter(Collider other)
     {
         playerSpeed = Player.GetComponent<Rigidbody>().velocity.magnitude;
-        Player.GetComponent<Rigidbody>().velocity = Vector3.up * playerSpeed * springModifier;
+        Player.GetComponent<Rigidbody>().velocity = transform.up * springBase + transform.up * playerSpeed * springModifier;
 
     }
 
