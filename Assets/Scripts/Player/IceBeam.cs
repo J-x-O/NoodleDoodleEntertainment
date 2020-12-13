4dbd174cd31267a3b2fb3e6f4693ae1f2ff06404
Felix <werner.felixx@gmx.de>
2020-12-13 04:18:11 +0100

    it's a slippery slope from here on

--- Assets/Scripts/Player/IceBeam.cs
+++ Assets/Scripts/Player/IceBeam.cs
@@ -9,13 +9,32 @@ namespace Player {
         [SerializeField] private float boostAmount;
         [SerializeField] [Range(0.0f, 1.0f)] private float resourceCost;
         [SerializeField] private ResourceManager resourceManager;
+        [SerializeField] private float iceDuration;
+        [SerializeField] private IceSheet iceSheetPrefab;
+        [SerializeField] private LayerMask collisionMask;
+        private readonly Collider[] cache = new Collider[8];
 
         private void Update() {
             var trans = transform;
             var origin = trans.position;
             var aimDirection = aim.AimDirection;
             var distance = maxDistance;
-            if (Physics.Raycast(origin, aimDirection, out var hitInfo, maxDistance)) distance = math.length(hitInfo.point - origin);
+            if (Physics.Raycast(origin, aimDirection, out var hitInfo, maxDistance, collisionMask)) {
+                var hitPosition = hitInfo.point;
+                distance = math.length(hitPosition - origin);
+                var size = Physics.OverlapSphereNonAlloc(hitPosition, 0.25f, cache, new LayerMask {value = 1024});
+                if (size > 0) {
+                    for (var i = 0; i < size; i++) {
+                        var iceSheet = cache[i].GetComponent<IceSheet>();
+                        iceSheet.Refresh(iceDuration);
+                    }
+                }
+                else {
+                    var iceSheet = Instantiate(iceSheetPrefab);
+                    iceSheet.Place(hitPosition, hitInfo.normal, iceDuration);
+                }
+            }
+
             float3 scale = trans.localScale;
             trans.localScale = new float3(scale.xy, distance);
             var cost = resourceCost * Time.deltaTime;
