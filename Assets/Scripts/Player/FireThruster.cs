9f82997541e54e997573fa13affc9ffd36d508ce
Jesco <JescoVogt@web.de>
2020-12-13 13:35:59 +0100

    Systems Somewhat Working Together

--- Assets/Scripts/Player/FireThruster.cs
+++ Assets/Scripts/Player/FireThruster.cs
@@ -10,20 +10,32 @@ namespace Player {
         [SerializeField] [Range(0.0f, 1.0f)] private float resourceCost;
         [SerializeField] private ResourceManager resourceManager;
 
+        private ParticleSystem particleSystem;
+        private bool boosting = false;
+
+        private void OnEnable() {
+            particleSystem = GetComponentInChildren<ParticleSystem>();
+            if (particleSystem == null) Debug.LogError("Add FireParticlePreset To FireThruster!");
+        }
+
         private void Update() {
-            var trans = transform;
-            var origin = trans.position;
-            var aimDirection = aim.AimDirection;
-            var distance = maxDistance;
-            if (Physics.Raycast(origin, aimDirection, out var hitInfo, maxDistance)) distance = math.length(hitInfo.point - origin);
-            float3 scale = trans.localScale;
-            trans.localScale = new float3(scale.xy, distance);
-            var cost = resourceCost * Time.deltaTime;
-            playerMovement.Boost(-aimDirection * resourceManager.UseFire(cost) * boostAmount);
+            if(boosting) {
+                var trans = transform;
+                var origin = trans.position;
+                var aimDirection = aim.aimDirection;
+                var distance = maxDistance;
+                if (Physics.Raycast(origin, aimDirection, out var hitInfo, maxDistance)) distance = math.length(hitInfo.point - origin);
+                float3 scale = trans.localScale;
+                trans.localScale = new float3(scale.xy, distance);
+                var cost = resourceCost * Time.deltaTime;
+                playerMovement.Boost(-aimDirection * resourceManager.UseFire(cost) * boostAmount);
+            }
         }
 
         public void Toggle(bool value) {
-            gameObject.SetActive(value); //TODO replace with particle system
+            if (value) particleSystem.Play();
+            else particleSystem.Stop();
+            boosting = value;
         }
     }
 }
--- Assets/Scripts/Player/IceBeam.cs
+++ Assets/Scripts/Player/IceBeam.cs
@@ -14,35 +14,47 @@ namespace Player {
         [SerializeField] private LayerMask collisionMask;
         private readonly Collider[] cache = new Collider[8];
 
+        private ParticleSystem particleSystem;
+        private bool boosting = false;
+
+        private void OnEnable() {
+            particleSystem = GetComponentInChildren<ParticleSystem>();
+            if (particleSystem == null) Debug.LogError("Add FireParticlePreset To FireThruster!");
+        }
+
         private void Update() {
-            var trans = transform;
-            var origin = trans.position;
-            var aimDirection = aim.AimDirection;
-            var distance = maxDistance;
-            if (Physics.Raycast(origin, aimDirection, out var hitInfo, maxDistance, collisionMask)) {
-                var hitPosition = hitInfo.point;
-                distance = math.length(hitPosition - origin);
-                var size = Physics.OverlapSphereNonAlloc(hitPosition, 0.25f, cache, new LayerMask {value = 1024});
-                if (size > 0) {
-                    for (var i = 0; i < size; i++) {
-                        var iceSheet = cache[i].GetComponent<IceSheet>();
-                        iceSheet.Refresh(iceDuration);
+            if(boosting) {
+                var trans = transform;
+                var origin = trans.position;
+                var aimDirection = aim.aimDirection;
+                var distance = maxDistance;
+                if (Physics.Raycast(origin, aimDirection, out var hitInfo, maxDistance, collisionMask)) {
+                    var hitPosition = hitInfo.point;
+                    distance = math.length(hitPosition - origin);
+                    var size = Physics.OverlapSphereNonAlloc(hitPosition, 0.25f, cache, new LayerMask { value = 1024 });
+                    if (size > 0) {
+                        for (var i = 0; i < size; i++) {
+                            var iceSheet = cache[i].GetComponent<IceSheet>();
+                            iceSheet.Refresh(iceDuration);
+                        }
+                    }
+                    else {
+                        var iceSheet = Instantiate(iceSheetPrefab);
+                        iceSheet.Place(hitPosition, hitInfo.normal, iceDuration);
                     }
                 }
-                else {
-                    var iceSheet = Instantiate(iceSheetPrefab);
-                    iceSheet.Place(hitPosition, hitInfo.normal, iceDuration);
-                }
-            }
 
-            float3 scale = trans.localScale;
-            trans.localScale = new float3(scale.xy, distance);
-            var cost = resourceCost * Time.deltaTime;
-            playerMovement.Boost(aimDirection * resourceManager.UseIce(cost) * boostAmount);
+                float3 scale = trans.localScale;
+                trans.localScale = new float3(scale.xy, distance);
+                var cost = resourceCost * Time.deltaTime;
+                playerMovement.Boost(aimDirection * resourceManager.UseIce(cost) * boostAmount);
+            }
         }
 
         public void Toggle(bool value) {
-            gameObject.SetActive(value); //TODO replace with particle system
+            if (value) particleSystem.Play();
+            else particleSystem.Stop();
+            boosting = value;
         }
     }
 }
