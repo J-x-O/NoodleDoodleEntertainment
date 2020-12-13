1fd2bec5ae68f38399a4655f20261eef7cdfc4c2
Ibrahim Kirschstein <ikirschs@hs-mittweida.de>
2020-12-13 14:43:25 +0100

    Spring Placeholder Function implemented

---
+++ Assets/Scripts/Level/Spring.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
+    [SerializeField] float springModifier;
+    private float playerSpeed;
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        playerSpeed = Player.GetComponent<Rigidbody>().velocity.magnitude;
+        Player.GetComponent<Rigidbody>().velocity = Vector3.up * playerSpeed * springModifier;
+
+    }
+
+}
